c0c4118602ea45a5ee2ff868df7985f020b85f1c
Roberto Sonnino <robertos@microsoft.com>
2017-12-03 20:32:10 -0800

    Enable XMLDoc generation. Fix XMLDoc issues

--- glTF-Toolkit/inc/AccessorUtils.h
+++ glTF-Toolkit/inc/AccessorUtils.h
@@ -17,12 +17,12 @@ namespace Microsoft::glTF::Toolkit
     public:
         /// <summary>
         /// Calculates the min and max values for an accessor according to the glTF 2.0 specification.
-        /// </summary>
         /// <param name="accessor">The accessor definition for which the min and max values will be calculated.</param>
         /// <param name="accessorContents">The raw data contained in the accessor.</param>
         /// <returns>
         /// A pair containing the min and max vectors for the accessor, in that order.
         /// </returns>
+        /// </summary>
         template <typename T>
         static std::pair<std::vector<float>, std::vector<float>> CalculateMinMax(const Accessor& accessor, const std::vector<T>& accessorContents)
         {
--- glTF-Toolkit/inc/GLTFTextureCompressionUtils.h
+++ glTF-Toolkit/inc/GLTFTextureCompressionUtils.h
@@ -33,7 +33,6 @@ namespace Microsoft::glTF::Toolkit
         /// <summary>Compresses a texture in a glTF from a WIC-readable format (PNG, JPEG, BMP, GIF, TIFF, HD Photo, ICO) 
         /// into a DDS with the appropriate compression.
         /// <para>If a dds extension already exists for this texture, do nothing.</para>
-        /// </summary>
         /// <param name="streamReader">The stream reader that will be used to get streams to each image from its URI.</param>
         /// <param name="doc">Input glTF document.</param>
         /// <param name="texture">Texture object that is contained in input document. If texture does not exist in document, 
@@ -44,7 +43,7 @@ namespace Microsoft::glTF::Toolkit
         /// <param name="retainOriginalImage">If true, retains the original image on the resulting glTF. If false, 
         /// replaces that image (making the glTF incompatible with most core glTF 2.0 viewers).</param>
         /// <returns>Returns a new GLTFDocument that contains a new reference to the compressed dds file added as part 
-        /// of the MSFT_texture_dds extension.</return>
+        /// of the MSFT_texture_dds extension.</returns>
         /// <example>
         /// Example Input:
         /// <code>
@@ -83,13 +82,13 @@ namespace Microsoft::glTF::Toolkit
         /// ]
         /// </code>
         /// </example>
+        /// </summary>
         static GLTFDocument CompressTextureAsDDS(const IStreamReader& streamReader, const GLTFDocument & doc, const Texture & texture, TextureCompression compression, const std::string& outputDirectory, bool generateMipMaps = true, bool retainOriginalImage = true);
 
         /// <summary>
-        /// Applies <see cref="CompressTextureForWindowsMR" /> to all textures in the document that are accessible via materials according to the 
+        /// Applies <see cref="CompressTextureAsDDS" /> to all textures in the document that are accessible via materials according to the 
         /// requirements of the Windows Mixed Reality home.
         /// <para>Normal textures get compressed with BC5, while baseColorTexture, occlusion, metallicRoughness and emissive textures get compressed with BC7.</para>
-        /// </summary>
         /// <param name="streamReader">The stream reader that will be used to get streams to each image from its URI.</param>
         /// <param name="doc">Input glTF document.</param>
         /// <param name="outputDirectory">The output directory to which compressed files should be saved.</param>
@@ -97,7 +96,8 @@ namespace Microsoft::glTF::Toolkit
         /// <param name="retainOriginalImage">If true, retains the original image on the resulting glTF. If false, 
         /// replaces that image (making the glTF incompatible with most core glTF 2.0 viewers).</param>
         /// <returns>Returns a new GLTFDocument that contains alternate textures for all applicable materials following the requirements of the Windows
-        /// Mixed Reality home using the MSFT_texture_dds extension.</return>
+        /// Mixed Reality home using the MSFT_texture_dds extension.</returns>
+        /// </summary>
         static GLTFDocument CompressAllTexturesForWindowsMR(const IStreamReader& streamReader, const GLTFDocument & doc, const std::string& outputDirectory, bool retainOriginalImages = true);
 
         /// <summary>
